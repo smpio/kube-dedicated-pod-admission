03f85308037fed42c08bc512af71e49f522efcbc
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-04-03 23:40:14 +0300

    fully working version

--- Dockerfile
+++ Dockerfile
@@ -13,5 +13,4 @@ RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags "-s -w"
 
 FROM scratch
 COPY --from=builder /go/src/github.com/smpio/kube-dedicated-pod-admission/kube-dedicated-pod-admission /
-COPY empty /tmp
 ENTRYPOINT ["/kube-dedicated-pod-admission"]
--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
+    "log"
     "flag"
-    "strings"
     "net/http"
     "io/ioutil"
     "crypto/tls"
     "encoding/json"
 
-    "github.com/golang/glog"
     "k8s.io/apimachinery/pkg/runtime"
     "k8s.io/apimachinery/pkg/runtime/serializer"
     corev1 "k8s.io/api/core/v1"
@@ -19,8 +18,10 @@ import (
     "k8s.io/client-go/rest"
 )
 
+// TODO: smp.io/dedicated (add option for this)
 const taintName string = "dedicated"
 const labelName string = "dedicated"
+const nsAnnotation string = "smp.io/force-dedicated-nodes"
 
 type operation struct {
     Op     string      `json:"op"`
@@ -59,7 +60,7 @@ func main() {
 func configTLS(CertFile string, KeyFile  string) *tls.Config {
     sCert, err := tls.LoadX509KeyPair(CertFile, KeyFile)
     if err != nil {
-        glog.Fatal(err)
+        log.Fatal(err)
     }
     return &tls.Config{
         Certificates: []tls.Certificate{sCert},
@@ -74,11 +75,12 @@ func mkServe(clientset *kubernetes.Clientset) func(http.ResponseWriter, *http.Re
                 body = data
             }
         }
+        log.Print(string(body))
 
         // verify the content type is accurate
         contentType := r.Header.Get("Content-Type")
         if contentType != "application/json" {
-            glog.Errorf("contentType=%s, expect application/json", contentType)
+            log.Printf("contentType=%s, expect application/json", contentType)
             return
         }
 
@@ -86,10 +88,10 @@ func mkServe(clientset *kubernetes.Clientset) func(http.ResponseWriter, *http.Re
         ar := v1beta1.AdmissionReview{}
         deserializer := codecs.UniversalDeserializer()
         if _, _, err := deserializer.Decode(body, nil, &ar); err != nil {
-            glog.Error(err)
+            log.Print(err)
             reviewResponse = toAdmissionResponse(err)
         } else {
-            reviewResponse = admit(ar)
+            reviewResponse = admit(ar, clientset)
         }
 
         response := v1beta1.AdmissionReview{}
@@ -103,10 +105,10 @@ func mkServe(clientset *kubernetes.Clientset) func(http.ResponseWriter, *http.Re
 
         resp, err := json.Marshal(response)
         if err != nil {
-            glog.Error(err)
+            log.Print(err)
         }
         if _, err := w.Write(resp); err != nil {
-            glog.Error(err)
+            log.Print(err)
         }
     }
 }
@@ -119,52 +121,91 @@ func toAdmissionResponse(err error) *v1beta1.AdmissionResponse {
     }
 }
 
-func admit(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
-    glog.Info("---1")
+func admit(ar v1beta1.AdmissionReview, clientset *kubernetes.Clientset) *v1beta1.AdmissionResponse {
     podResource := metav1.GroupVersionResource{Group: "", Version: "v1", Resource: "pods"}
     if ar.Request.Resource != podResource {
-        glog.Errorf("expect resource to be %s", podResource)
+        log.Printf("expected resource to be %s", podResource)
         return nil
     }
 
-    glog.Info("---2")
+    if ar.Request.Operation != "CREATE" {
+        log.Printf("expected operation to be %s", "CREATE")
+        return nil
+    }
+
     raw := ar.Request.Object.Raw
     pod := corev1.Pod{}
     deserializer := codecs.UniversalDeserializer()
     if _, _, err := deserializer.Decode(raw, nil, &pod); err != nil {
-        glog.Error(err)
+        log.Print(err)
         return toAdmissionResponse(err)
     }
 
-    glog.Info("---3")
     reviewResponse := v1beta1.AdmissionResponse{}
     reviewResponse.Allowed = true
-    if strings.HasPrefix(pod.Name, "to-be-mutated") {
-        glog.Info("---4")
-        patch, err := json.Marshal(makePatch(&pod))
-        if err != nil {
-            glog.Error(err)
-            return toAdmissionResponse(err)
+
+    if label, ok := pod.Labels["run"]; ok {
+        if label == "to-be-mutated" {
+            operations := makePatch(&pod, ar.Request.Namespace, clientset)
+
+            if len(operations) != 0 {
+                patch, err := json.Marshal(operations)
+                if err != nil {
+                    log.Print(err)
+                    return toAdmissionResponse(err)
+                }
+
+                log.Print(string(patch))
+                reviewResponse.Patch = patch
+                pt := v1beta1.PatchTypeJSONPatch
+                reviewResponse.PatchType = &pt
+            }
         }
-        glog.Info("---5")
-        glog.Info(string(patch))
-        reviewResponse.Patch = patch
-        pt := v1beta1.PatchTypeJSONPatch
-        reviewResponse.PatchType = &pt
     }
+
     return &reviewResponse
 }
 
-func makePatch(pod *corev1.Pod) []*operation {
+func makePatch(pod *corev1.Pod, namespace string, clientset *kubernetes.Clientset) []*operation {
     ops := []*operation{}
+
     if !hasTolerationEffect(pod, corev1.TaintEffectNoExecute) {
-        ops = append(ops, makeOperation(corev1.TaintEffectNoExecute, pod.Namespace))
+        ops = append(ops, makeTolerationOperation(corev1.TaintEffectNoExecute, namespace))
     }
     if !hasTolerationEffect(pod, corev1.TaintEffectNoSchedule) {
-        ops = append(ops, makeOperation(corev1.TaintEffectNoSchedule, pod.Namespace))
+        ops = append(ops, makeTolerationOperation(corev1.TaintEffectNoSchedule, namespace))
     }
 
-    // _, err = clientset.CoreV1().Pods("default").Get("example-xxxxx", metav1.GetOptions{})
+    _, ok := pod.Spec.NodeSelector[labelName]
+    if ok {
+        return ops
+    }
+
+    ns, err := clientset.CoreV1().Namespaces().Get(namespace, metav1.GetOptions{})
+    if err != nil {
+        log.Print(err)
+        return ops
+    }
+
+    log.Print(ns.Annotations)
+    annotation, ok := ns.Annotations[nsAnnotation]
+    if !ok || annotation != "true" {
+        return ops
+    }
+
+    if len(pod.Spec.NodeSelector) == 0 {
+        ops = append(ops, &operation{
+            Op: "add",
+            Path: "/spec/nodeSelector",
+            Value: map[string]string{labelName: namespace},
+        })
+    } else {
+        ops = append(ops, &operation{
+            Op: "add",
+            Path: "/spec/nodeSelector/" + labelName,
+            Value: namespace,
+        })
+    }
 
     return ops
 }
@@ -179,7 +220,7 @@ func hasTolerationEffect(pod *corev1.Pod, effect corev1.TaintEffect) bool {
     return false
 }
 
-func makeOperation(effect corev1.TaintEffect, namespace string) *operation {
+func makeTolerationOperation(effect corev1.TaintEffect, namespace string) *operation {
     return &operation{
         Op: "add",
         Path: "/spec/tolerations/0",
@@ -195,11 +236,11 @@ func makeOperation(effect corev1.TaintEffect, namespace string) *operation {
 func getClient() *kubernetes.Clientset {
     config, err := rest.InClusterConfig()
     if err != nil {
-        glog.Fatal(err)
+        log.Fatal(err)
     }
     clientset, err := kubernetes.NewForConfig(config)
     if err != nil {
-        glog.Fatal(err)
+        log.Fatal(err)
     }
     return clientset
 }
